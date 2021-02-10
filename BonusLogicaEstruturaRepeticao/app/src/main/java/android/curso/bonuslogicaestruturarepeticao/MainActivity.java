dd1008da47f0386a2145fe180af9b1461d8d8b1b
Andre <gmlandreribeiro@gmail.com>
2021-02-10 19:29:20 -0300

    Atualização Android Studio 4.1.2 - API 30

--- BonusLogicaEstruturaRepeticao/app/src/main/java/android/curso/bonuslogicaestruturarepeticao/MainActivity.java
+++ BonusLogicaEstruturaRepeticao/app/src/main/java/android/curso/bonuslogicaestruturarepeticao/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         List<String> linguagens = new ArrayList<>();
 
-
         popularLista(linguagens);
 
         // Primeira opção..: for
